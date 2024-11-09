2c667ae3802b50af627f1c81b8334b6607e25d3d
martoast <alexmartos96@gmail.com>
2024-11-08 22:45:32 -0800

    trying with old nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  nitro: {
-    preset: 'netlify',
-  },
-  modules: ['@nuxtjs/tailwindcss']
+  css: ['~/assets/css/fonts.css'],
+  modules: ['@nuxtjs/tailwindcss'],
+  
 })
